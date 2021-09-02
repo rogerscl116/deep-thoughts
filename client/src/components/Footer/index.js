f6c9e0027c3d3d63a3f1f67e910b074446e81c5f
charity.rogers <rogerscl116@gmail.com>
2021-09-01 22:01:22 -0500

    reinstall dependencies to fix profile page bug

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2021 by Charity Rogers</div>
+      <div className="container">
+        &copy;{new Date().getFullYear()} by Charity Rogers</div>
     </footer>
   );
 };
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import Auth from '../../utils/auth';
 
 const Header = () => {
@@ -12,21 +13,21 @@ const Header = () => {
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
-        <h1>Deep Thoughts</h1>
+          <h1>Deep Thoughts</h1>
         </Link>
 
         <nav className="text-center">
           {Auth.loggedIn() ? (
             <>
-            <Link to="/profile">Me</Link>
-            <a href="/" onClick={logout}>
-              Logout
-            </a>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
             </>
           ) : (
             <>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
             </>
           )}
         </nav>
